99762a425e8cde578bfa37f66c6c8780fa3c5831
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-02-22 09:50:19 +0530

    Create Play With Zeros.java

---
+++ Play With Zeros.java
@@ -0,0 +1,17 @@
+Question Link: https://my.newtonschool.co/playground/code/w1fukw4zw417
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        System.out.printf("%04d\n", N);
+    }
+}
