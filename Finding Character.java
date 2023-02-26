37a8eba16c1c4f55cf6556453e1d8a2e6145631f
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-02-26 23:17:37 +0530

    Update Finding Character.java

--- Finding Character.java
+++ Finding Character.java
@@ -1,1 +1,23 @@
 Question Link: https://my.newtonschool.co/playground/code/dae1o5tku1ay
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int x = sc.nextInt();
+
+        int pos = (x-1) % (26*n);
+        char c = (char) ('A' + pos/n);
+
+        System.out.println(c);
+    }
+}
