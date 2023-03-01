ec7163ebcf03fdbc64498865ab95e7d2e1b2c671
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-01 14:32:25 +0530

    Create Alexa and Air Conditioner.java

---
+++ Alexa and Air Conditioner.java
@@ -0,0 +1,21 @@
+Question Link: https://my.newtonschool.co/playground/code/ywtkr4t65pb8
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int X = sc.nextInt();
+        if (X >= 30) {
+        System.out.println("Yes");
+        }
+        else {
+        System.out.println("No");
+        }
+    }
+}
