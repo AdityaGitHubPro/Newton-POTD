6e0179b5d87d300ea0e74fdbc64512dc5ce7ddb2
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-07 09:57:35 +0530

    Create Spinning Slots.java

---
+++ Spinning Slots.java
@@ -0,0 +1,20 @@
+Question Link: https://my.newtonschool.co/playground/code/zsfkbr1095ia
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        if (s.charAt(0) == s.charAt(1) && s.charAt(1) == s.charAt(2)) {
+            System.out.println("Won");
+        } else {
+            System.out.println("Lost");
+        }
+    }
+}
