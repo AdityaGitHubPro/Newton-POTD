28aa44480961fcf764b0bb97286c0c88f374c3e0
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-14 09:22:55 +0530

    Create Emilia and Bitwise Xor.java

---
+++ Emilia and Bitwise Xor.java
@@ -0,0 +1,19 @@
+Question Link: https://my.newtonschool.co/playground/code/ach1kyxiec9e
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int ans = a ^ b; 
+        System.out.println(ans); 
+    }
+}
