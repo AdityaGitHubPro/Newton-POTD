7f1713543b161d8901d564dc0467f85743655ea8
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-18 11:03:43 +0530

    Update Palindromic String.java

--- Palindromic String.java
+++ Palindromic String.java
@@ -7,27 +7,35 @@ import java.util.*; // contains Collections framework
 // don't change the name of this class
 // you can add inner classes if needed
 class Main {
+    static boolean checkPalindrome(String str,int l,int r){
+        while(l<r){
+            if(str.charAt(l)!=str.charAt(r)){
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    static boolean helper(String str,int l,int r){
+        while(l<r){
+            if(str.charAt(l)=='a'&&str.charAt(r)=='a'){
+                l++;
+                r--;
+            }
+            else break;
+        }
+        while(l<r&&str.charAt(r)=='a'){
+            r--;
+        }
+        return checkPalindrome(str,l,r);
+    } 
     public static void main (String[] args) {
         // Your code here
-        Scanner sc = new Scanner(System.in);
-        String s = sc.nextLine();
-        int n = s.length();
-        boolean canBePalindrome = true;
-        for (int i = 0; i < n / 2; i++) {
-            if (s.charAt(i) != s.charAt(n - i - 1)) {
-                canBePalindrome = false;
-                break;
-            }
-        }
-        if (canBePalindrome) {
-            System.out.println("Yes");
-        } else {
-            String reversedS = new StringBuilder(s).reverse().toString();
-            if ((reversedS + s).equals(new StringBuilder(reversedS + s).reverse().toString())) {
-                System.out.println("No");
-            } else {
-                System.out.println("Yes");
-            }
-        }
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        boolean result=helper(str,0,str.length()-1);
+        if(result)System.out.print("Yes");
+        else System.out.print("No");
     }
 }
