09f91d9a7e2e20c61e59a0e40fa1c170af97a2bf
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-03-29 18:02:46 +0530

    Create Fitness.java

---
+++ Fitness.java
@@ -0,0 +1,18 @@
+Question Link: https://my.newtonschool.co/playground/code/luhar9q1i9w2
+
+
+
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc=new Scanner(System.in);
+        int x=sc.nextInt();
+        int ans=x*10;
+        System.out.println(ans);
+    }
+}
