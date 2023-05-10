5825d1850662e2c6cd8dbbddd978e80ef6fee763
ADITYA RAJENDRA  NARNAWARE <95174961+AdityaGitHubPro@users.noreply.github.com>
2023-05-10 08:31:48 +0530

    Create Aditi and Her Business.java

---
+++ Aditi and Her Business.java
@@ -0,0 +1,33 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+    public static void main (String[] args) {
+        // Your code here
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] a = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+
+        int ml = 1;
+        int cl = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] >= a[i-1]) {
+                ++;
+            }
+            else {
+                maxLength = Math.max(maxLength, currentLength);
+                currentLength = 1;
+            }
+        }
+        maxLength = Math.max(maxLength, currentLength);
+        System.out.println(maxLength);
+    }
+}
